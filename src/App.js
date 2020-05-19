8c870cc11b2c3753914bd8df76a5d7a9c9f11864
r00bal <hal9000@MacBook-Pro-r00bal.local>
2020-05-19 23:02:40 +0200

    add app component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { filter, groupBy, max, min, map, toNumber, trim, toArray } from 'lodash';
 import './App.css';
 
-function App({hello}) {
+const _groupBy = (data, category = 'category') => {
+  data = filter(data, function (o) {
+    return o.showinmenu.trim().toLowerCase() == "yes";
+  });
+  return groupBy(data, category);
+};
+const maxPrice = (items) => {
+  return max(getPrices(items));
+};
+const minPrice = (items) => {
+  return min(getPrices(items));
+};
+const getPrices = (items, value = 'prices') => {
+  let prices = map(items, 'prices');
+  return prices.map(function (item) {
+    return toNumber(trim(item.trim(), '$'));
+  });
+}
+
+function App({ hello }) {
+  const [menu, setMenu] = useState();
+  const [restaurant, setRestaurant] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      const resultMenu = await axios(
+        'https://sheetdb.io/api/v1/3x3bl9v7indtm?sheet=menu&fbclid=IwAR18nVF2gXhgk9B6hHVpoBruWiIuWotUt4BQeEs4jazF8sPaW2qBkYCvfno',
+      );
+      const { data } = resultMenu;
+
+      setMenu(groupBy((data), 'maincategory'));
+    }
+    fetchData()
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      const resultRestaurant = await axios(
+        'https://sheetdb.io/api/v1/3x3bl9v7indtm?sheet=location&fbclid=IwAR18nVF2gXhgk9B6hHVpoBruWiIuWotUt4BQeEs4jazF8sPaW2qBkYCvfno',
+      );
+      setRestaurant(resultRestaurant.data[0]);
+    }
+    fetchData()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         {hello}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="sheet">
+      <h1>{restaurant && restaurant.restaurant}</h1>
+      <p style={{ 'textAlign': 'center' }}>{restaurant && restaurant.address}</p>
+
+      {menu && Object.keys(menu).map((maincategory, i) => {
+        console.log(maincategory)
+        const category = _groupBy(menu[maincategory]);
+        console.log(category)
+        return (
+          <div>
+
+            <h2>{maincategory}</h2>
+            {Object.keys(category).map((item, j) => (
+              <div>
+                <p class="pricerange">{category[item].length} {item} Items - ${minPrice(category[item])} to ${maxPrice(category[item])}</p>
+                <ul style={{ 'paddingBottom': '0' }}>
+                  {category[item].map(({ title, description, prices }) => (
+                    <li>
+                      <strong>{title}</strong> - {description} - PRICE {prices}
+                    </li>
+                  ))}
+
+                </ul>
+              </div>
+            ))}
+          </div>
+        )
+      })}
     </div>
-  );
 }
 
 export default App;
